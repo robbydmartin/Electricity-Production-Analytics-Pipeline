094825c8aff44b0f7086bddc76d0b891ec3bcc42
robbydmartin <robertdm80@yahoo.com>
2026-03-25 10:30:10 -0400

    Update with API call

---
+++ data/electricity_api.py
@@ -0,0 +1,26 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+def poll_api():
+
+    load_dotenv()
+
+    api_base_url = os.getenv("BASE_URL")
+    api_key = os.getenv("API_KEY")
+    url = f"{api_base_url}?api_key={api_key}&frequency=hourly&data[0]=value&start=2026-02-22T00&end=2026-02-28T00&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=500"
+
+    try:
+
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            print("API call sucessful")
+            data = response.json()
+            return data
+        else:
+            print(f"Request failed with status code: {response.status_code}")
+
+    except Exception as e:
+        print(f"[ERROR] {e}")
+        raise
---
+++ main.py
@@ -0,0 +1,9 @@
+
+from data.electricity_api import poll_api
+
+def main():
+
+    poll_api()
+
+if __name__ == '__main__':
+    main()
